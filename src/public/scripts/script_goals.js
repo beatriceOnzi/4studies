4ce0dfbe7e235632a0c183a5f91a08c5a938da90
beatrice <biaonca1200@gmail.com>
2026-09-21 19:38:08 -0300

    fix new goal color

--- src/public/scripts/script_goals.js
+++ src/public/scripts/script_goals.js
@@ -63,8 +63,8 @@ function createLi(data, goalList) {
 
     const template = document.createElement('template');
     template.innerHTML = `
-        <li data-id="${data.id}" class="flex group font-medium text-3xl text-wrap tracking-widest text-branco">
-            <svg data-type="${type}" xmlns="http://www.w3.org/2000/svg" class="group-has-[a:hover]:text-pink-darker mt-1.5 mx-2 text-branco check_icon_size"
+        <li data-id="${data.id}" class="flex group font-medium text-3xl text-wrap tracking-widest text-white">
+            <svg data-type="${type}" xmlns="http://www.w3.org/2000/svg" class="group-has-[a:hover]:text-pink-darker mt-1.5 mx-2 text-white check_icon_size"
                 viewBox="0 0 24 24">
                 <g id="not_completed" fill="currentColor">
                     <path d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14Z"/>
